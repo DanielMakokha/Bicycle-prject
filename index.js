2caf4df5a4aad2bbc34c32b6a192714cdfc725cf
DanielMakokha <danielmakokhan@gmail.com>
2023-08-16 07:35:35 +0300

    query string

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const http=require('http');
+const url=require('url')
 server=http.createServer((req,res)=>{
-    // console.log('Server is now running');
+
+    console.log(req.headers);
+    const myUrl= new URL(req.url,`http://$req.headers.host/`);
+    const pathname=myUrl.pathname;
+    const id=myUrl.searchParams.get('id')
+    console.log(pathname,id);
+
     res.writeHead(200,{"content-type":"text/html"})
     res.end("<h1>Welcome to the bicycle project</h1>")
 });
