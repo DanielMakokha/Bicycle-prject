55778c747a88ac4d2d4aa965dd7ef3b9bdfcfb76
DanielMakokha <danielmakokhan@gmail.com>
2023-08-22 10:12:29 +0300

    html rendering

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const http=require('http');
 const url=require('url')
-server=http.createServer((req,res)=>{
+const fs=require('fs').promises;
+server=http.createServer(async(req,res)=>{
 
     console.log(req.headers);
     const myUrl= new URL(req.url,`http://$req.headers.host/`);
     const pathname=myUrl.pathname;
     const id=myUrl.searchParams.get('id')
-    console.log(pathname,id);
+    if (pathname ==='/') {
+        const html=await fs.readFile('./view/bicycles.html','utf-8')
+        res.writeHead(200,{"content-type":"text/html"})
+        res.end(html)
+    }else if(pathname==='/bicycle' && id>=0 && id<=5){
+        const html=await fs.readFile('./view/overview.html','utf-8');
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(html);
+    }else{
+        res.writeHead(404,{"content-type":"text/html"})
+        res.end('<h1>File not found</h1>')
+    }
 
     res.writeHead(200,{"content-type":"text/html"})
-    res.end("<h1>Welcome to the bicycle project</h1>")
 });
 
 server.listen(3000);
