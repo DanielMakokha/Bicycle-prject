f95240a78cb440289de9470f3d2ec7bec51e5416
DanielMakokha <danielmakokhan@gmail.com>
2023-08-29 16:38:29 +0300

    rendering html

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const http=require('http');
 const url=require('url')
 const fs=require('fs').promises;
+const bicycles=require('./data/data.json')
+console.log(bicycles);
+
 server=http.createServer(async(req,res)=>{
 
-    console.log(req.headers);
+    // console.log(req.headers);
     const myUrl= new URL(req.url,`http://$req.headers.host/`);
     const pathname=myUrl.pathname;
     const id=myUrl.searchParams.get('id')
@@ -14,7 +17,20 @@ server=http.createServer(async(req,res)=>{
         res.writeHead(200,{"content-type":"text/html"})
         res.end(html)
     }else if(pathname==='/bicycle' && id>=0 && id<=5){
-        const html=await fs.readFile('./view/overview.html','utf-8');
+        let html=await fs.readFile('./view/overview.html','utf-8');
+        const bicycle=bicycles.find((b) =>b.id===id);
+        console.log(bicycle);
+        html=html.replace(/<%IMAGE%>/g, bicycle.image)
+        html=html.replace(/<%NAME%>/g, bicycle.name);
+
+        let price=bicycle.originalPrice;
+        if (bicycle.hasDiscount) {
+            price=(price*(100-bicycle.discount))/100
+        }
+        html=html.replace(/<%NEWPRICE%>/g,`$${price}`)
+
+
+
         res.writeHead(200,{"content-type":"text/html"});
         res.end(html);
     }
