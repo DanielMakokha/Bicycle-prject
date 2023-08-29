f1d1123bfc5af57c0f6852dbe69b33f3a6655893
DanielMakokha <danielmakokhan@gmail.com>
2023-08-29 14:39:57 +0300

    html-css render

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ server=http.createServer(async(req,res)=>{
     const myUrl= new URL(req.url,`http://$req.headers.host/`);
     const pathname=myUrl.pathname;
     const id=myUrl.searchParams.get('id')
+
+    console.log(req.url);
     if (pathname ==='/') {
         const html=await fs.readFile('./view/bicycles.html','utf-8')
         res.writeHead(200,{"content-type":"text/html"})
@@ -15,7 +17,13 @@ server=http.createServer(async(req,res)=>{
         const html=await fs.readFile('./view/overview.html','utf-8');
         res.writeHead(200,{"content-type":"text/html"});
         res.end(html);
-    }else{
+    }
+    else if (/\.(png)$/i.test(res.url)) {
+      const image=await fs.readFile('./public/image/${req.url.slice(1)}') 
+      res.writeHead(200,{"content-type":"image/png"})
+      res.end(image)
+    }
+    else{
         res.writeHead(404,{"content-type":"text/html"})
         res.end('<h1>File not found</h1>')
     }
@@ -23,4 +31,4 @@ server=http.createServer(async(req,res)=>{
     res.writeHead(200,{"content-type":"text/html"})
 });
 
-server.listen(3000);
+server.listen(3001);
